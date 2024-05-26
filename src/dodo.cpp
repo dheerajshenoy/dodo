8ed68e719975f6e2e9590a522182521f6eb0b0de
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2024-05-26 19:11:17 +0530

    updated files

--- src/dodo.cpp
+++ src/dodo.cpp
@@ -1,96 +1,184 @@
 #include "dodo.hpp"
-#include "qlogging.h"
 
 Dodo::Dodo(QWidget *parent)
 {
-    ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
     m_layout->addWidget(m_label);
     m_widget->setLayout(m_layout);
     m_layout->setAlignment(Qt::AlignmentFlag::AlignCenter);
-    m_label->show();
     this->setCentralWidget(m_widget);
-    this->show();
     INIT_PDF();
+    SetKeyBinds();
+    Open("/home/neo/test.pdf", 15);
+    this->show();
 }
 
-int Dodo::INIT_PDF()
+void Dodo::SetKeyBinds()
 {
+    QShortcut *kb_zoomin = new QShortcut(QKeySequence("="), this, [=]() {
+        this->Zoom(+5);
+    });
+
+    QShortcut *kb_zoomout = new QShortcut(QKeySequence("-"), this, [=]() {
+        this->Zoom(-5);
+    });
+
+    QShortcut *kb_zoomreset = new QShortcut(QKeySequence("+"), this, [=]() {
+        this->ZoomReset();
+    });
+
+    QShortcut *kb_open = new QShortcut(QKeySequence("Ctrl+o"), this, [=]() {
+        QString filename = QFileDialog::getOpenFileName(this, "Open file", nullptr, "PDF Files (*.pdf);;");
+        this->Open(filename);
+    });
+
+    QShortcut *kb_next_page = new QShortcut(QKeySequence("j"), this, [=]() {
+        this->GotoPage(1);
+    });
+
+    QShortcut *kb_prev_page = new QShortcut(QKeySequence("k"), this, [=]() {
+        this->GotoPage(-1);
+    });
+}
+
+void Dodo::GotoPage(int pinterval)
+{
+    if (m_page_number + pinterval > m_page_count)
+    {
+        return;
+    }
+
+    m_page_number += pinterval;
+    Render();
+}
+
+void Dodo::Render()
+{
+    m_ctm = fz_scale(m_zoom / 100, m_zoom / 100);
+    m_ctm = fz_pre_rotate(m_ctm, m_rotate);
+
+    /* Render page to an RGB pixmap. */
+    fz_try(m_ctx)
+    {
+        m_pix = fz_new_pixmap_from_page_number(m_ctx, m_doc, m_page_number, m_ctm, fz_device_rgb(m_ctx), 0);
+        QImage img(m_pix->samples, m_pix->w, m_pix->h, m_pix->stride, QImage::Format_RGB888);
+        m_label->setPixmap(QPixmap::fromImage(img));
+    }
+    fz_catch(m_ctx)
+    {
+        fprintf(stderr, "cannot render page: %s\n", fz_caught_message(m_ctx));
+        fz_drop_document(m_ctx, m_doc);
+        fz_drop_context(m_ctx);
+        return;
+    }
+}
+
+void Dodo::ZoomReset()
+{
+    m_zoom = 100.0f;
+    Render();
+}
+
+void Dodo::Zoom(float r)
+{
+    m_zoom += r;
+    Render();
+}
+
+bool Dodo::INIT_PDF()
+{
+    return true;
+}
+
+bool Dodo::Open(QString filename, int page_number)
+{
+
+    m_ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+
     /* Create a context to hold the exception stack and various caches. */
-    if (!ctx)
+    if (!m_ctx)
     {
         fprintf(stderr, "cannot create mupdf context\n");
-        return EXIT_FAILURE;
+        return false;
     }
 
     /* Register the default file types to handle. */
-    fz_try(ctx)
-    fz_register_document_handlers(ctx);
-    fz_catch(ctx)
+    fz_try(m_ctx)
     {
-        fprintf(stderr, "cannot register document handlers: %s\n", fz_caught_message(ctx));
-        fz_drop_context(ctx);
-        return EXIT_FAILURE;
+        fz_register_document_handlers(m_ctx);
+    }
+    fz_catch(m_ctx)
+    {
+        fz_report_error(m_ctx);
+        fprintf(stderr, "cannot register document handlers: %s", fz_caught_message(m_ctx));
+        fz_drop_context(m_ctx);
+        return false;
     }
 
-    /* Open the document. */
-    fz_try(ctx)
-        doc = fz_open_document(ctx, input.c_str());
-    fz_catch(ctx)
+    m_filename = filename.toStdString();
+
+    // Open the document
+    fz_try(m_ctx)
     {
-        fprintf(stderr, "cannot open document: %s\n", fz_caught_message(ctx));
-        fz_drop_context(ctx);
-        return EXIT_FAILURE;
+        m_doc = fz_open_document(m_ctx, m_filename.c_str());
+    }
+    fz_catch(m_ctx)
+    {
+        fprintf(stderr, "cannot open document: %s\n", fz_caught_message(m_ctx));
+        fz_drop_context(m_ctx);
+        fz_drop_document(m_ctx, m_doc);
+        return false;
     }
 
-    /* Count the number of pages. */
-    fz_try(ctx)
+    // Count the number of pages
+    fz_try(m_ctx)
     {
-        page_count = fz_count_pages(ctx, doc);
-        qDebug() << page_count;
+        m_page_count = fz_count_pages(m_ctx, m_doc);
     }
-    fz_catch(ctx)
+    fz_catch(m_ctx)
     {
-        fprintf(stderr, "cannot count number of pages: %s\n", fz_caught_message(ctx));
-        fz_drop_document(ctx, doc);
-        fz_drop_context(ctx);
-        return EXIT_FAILURE;
+        fprintf(stderr, "cannot count number of pages: %s\n", fz_caught_message(m_ctx));
+        fz_drop_document(m_ctx, m_doc);
+        fz_drop_context(m_ctx);
+        return false;
     }
 
-    if (page_number < 0 || page_number >= page_count)
+    if (page_number < 0 || page_number > m_page_count)
     {
-        fprintf(stderr, "page number out of range: %d (page count %d)\n", page_number + 1, page_count);
-        fz_drop_document(ctx, doc);
-        fz_drop_context(ctx);
-        return EXIT_FAILURE;
+        fprintf(stderr, "incorrect page number");
+        fz_drop_document(m_ctx, m_doc);
+        fz_drop_context(m_ctx);
+        return false;
     }
+
+    m_page_number = page_number;
 
     /* Compute a transformation matrix for the zoom and rotation desired. */
     /* The default resolution without scaling is 72 dpi. */
-    ctm = fz_scale(zoom / 100, zoom / 100);
-    ctm = fz_pre_rotate(ctm, rotate);
+    m_ctm = fz_scale(m_zoom / 100, m_zoom / 100);
+    m_ctm = fz_pre_rotate(m_ctm, m_rotate);
 
     /* Render page to an RGB pixmap. */
-    fz_try(ctx)
+    fz_try(m_ctx)
     {
-        pix = fz_new_pixmap_from_page_number(ctx, doc, 15, ctm, fz_device_rgb(ctx), 0);
-        QImage img(pix->samples, pix->w, pix->h, pix->stride, QImage::Format_RGB888);
+        m_pix = fz_new_pixmap_from_page_number(m_ctx, m_doc, m_page_number, m_ctm, fz_device_rgb(m_ctx), 0);
+        QImage img(m_pix->samples, m_pix->w, m_pix->h, m_pix->stride, QImage::Format_RGB888);
         m_label->setPixmap(QPixmap::fromImage(img));
     }
-    fz_catch(ctx)
+    fz_catch(m_ctx)
     {
-        fprintf(stderr, "cannot render page: %s\n", fz_caught_message(ctx));
-        fz_drop_document(ctx, doc);
-        fz_drop_context(ctx);
-        return EXIT_FAILURE;
+        fprintf(stderr, "cannot render page: %s\n", fz_caught_message(m_ctx));
+        fz_drop_document(m_ctx, m_doc);
+        fz_drop_context(m_ctx);
+        return false;
     }
 
-    return EXIT_SUCCESS;
+    return true;
 }
 
 Dodo::~Dodo()
 {
     /* Clean up. */
-    fz_drop_pixmap(ctx, pix);
-    fz_drop_document(ctx, doc);
-    fz_drop_context(ctx);
+    fz_drop_pixmap(m_ctx, m_pix);
+    fz_drop_document(m_ctx, m_doc);
+    fz_drop_context(m_ctx);
 }
--- src/dodo.hpp
+++ src/dodo.hpp
@@ -1,6 +1,9 @@
 #include <mupdf/fitz.h>
 #include <qt6/QtCore/QDebug>
+#include <qt6/QtGui/QShortcut>
+#include <qt6/QtGui/QKeySequence>
 #include <qt6/QtWidgets/QApplication>
+#include <qt6/QtWidgets/QFileDialog>
 #include <qt6/QtWidgets/QMainWindow>
 #include <qt6/QtWidgets/QVBoxLayout>
 #include <qt6/QtWidgets/QLabel>
@@ -14,18 +17,25 @@ class Dodo : public QMainWindow {
 public:
     Dodo(QWidget *parent = nullptr);
     ~Dodo();
-    int INIT_PDF();
-
+    bool Open(QString filename, int page_number = 0);
+    bool INIT_PDF();
+    void SetKeyBinds();
+    void Zoom(float rate);
+    void ZoomReset();
+    void Render();
+    void GotoPage(int pinterval);
+    
 private:
 
-	fz_context *ctx;
-	fz_document *doc;
-	fz_pixmap *pix;
-	int page_number = 1, page_count;
-	float zoom = 400.0f, rotate = 0.0f;
-    std::string input = "/home/neo/test.pdf";
-	fz_matrix ctm;
-	int x, y;
+	fz_context *m_ctx;
+	fz_document *m_doc;
+	fz_pixmap *m_pix;
+    fz_page *m_page;
+	int m_page_number = 1, m_page_count;
+	float m_zoom = 100.0f, m_rotate = 0.0f;
+    std::string m_filename = "/home/neo/test.pdf";
+	fz_matrix m_ctm;
+	int m_x, m_y;
 
     QLabel *m_label = new QLabel();
     QVBoxLayout *m_layout = new QVBoxLayout();
