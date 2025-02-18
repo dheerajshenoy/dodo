44e3244c55c1b7a90fa8c8d0d3d3f2f3f71dd3d9
Dheeraj Shenoy <dheerajshenoy22@gmail.com>
2025-02-18 18:43:23 +0530

    Start from scratch

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "dodo.cpp"
-
-int main(int argc, char **argv)
-{
-    QApplication app(argc, argv);
-    Dodo d(argc, argv);
-
-    app.exec();
-}
--- src/result.hpp
+++
@@ -1,8 +0,0 @@
-#include <qt6/QtCore/QString>
-
-typedef struct Result
-{
-    bool status;
-    QString text;
-} Result;
-
