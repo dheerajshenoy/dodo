29248be6578345b87039eebabf1d834bcbebe724
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-05-19 10:36:17 +0530

    Add abort atomic flag

--- src/RenderWorker.hpp
+++ src/RenderWorker.hpp
@@ -1,6 +1,7 @@
 #include <QObject>
 #include <poppler/qt6/poppler-qt6.h>
 #include <QImage>
+#include <atomic>
 
 class RenderWorker : public QObject {
     Q_OBJECT
@@ -12,18 +13,29 @@ class RenderWorker : public QObject {
     : m_doc(doc), m_pageno(pageno), m_dpi_x(dpi_x), m_dpi_y(dpi_y)
     {}
 
+    void abort() noexcept { m_abort.store(true); }
+
     signals:
         void finished(int pageno, QImage image);
 
+
 public slots:
-    void process() {
+    void process() noexcept {
+        if (m_abort.load())
+            return emit finished(m_pageno, QImage());
+
         auto page = m_doc->page(m_pageno);
+
         if (!page) {
             emit finished(m_pageno, QImage()); // null image
             return;
         }
 
         QImage image = page->renderToImage(m_dpi_x, m_dpi_y);
+
+        if (m_abort.load())
+            return emit finished(m_pageno, QImage());
+
         emit finished(m_pageno, image);
     }
 
@@ -32,5 +44,6 @@ private:
     int m_pageno;
     float m_dpi_x;
     float m_dpi_y;
+    std::atomic<bool> m_abort;
 };
 
