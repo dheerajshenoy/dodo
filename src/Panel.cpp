7d483ac0bac034e8345351b1045ac31e5252d028
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-05-19 21:27:57 +0530

    Search Mode Additions

--- src/Panel.cpp
+++ src/Panel.cpp
@@ -10,10 +10,22 @@ void Panel::initGui() noexcept
 {
     this->setLayout(m_layout);
 
+    // Left widgets
     m_layout->addWidget(m_filename_label);
+
     m_layout->addStretch();
+
+    // Right widgets
+    m_layout->addWidget(new QLabel("Page: "));
     m_layout->addWidget(m_pageno_label);
+    m_layout->addWidget(new QLabel("/"));
     m_layout->addWidget(m_totalpage_label);
+
+    m_layout->addWidget(m_search_label);
+    m_layout->addWidget(m_search_index_label);
+    m_layout->addWidget(m_search_count_label);
+
+    this->setSearchMode(false);
 }
 
 
@@ -30,4 +42,22 @@ void Panel::setFileName(const QString &name) noexcept
 void Panel::setPageNo(const unsigned int &pageno) noexcept
 {
     m_pageno_label->setText(QString::number(pageno));
+}
+
+
+void Panel::setSearchCount(const unsigned int &count) noexcept
+{
+    m_search_count_label->setText(QString::number(count));
+}
+
+void Panel::setSearchIndex(const unsigned int &index) noexcept
+{
+    m_search_index_label->setText(QString::number(index));
+}
+
+void Panel::setSearchMode(const bool &state) noexcept
+{
+    m_search_label->setVisible(state);
+    m_search_index_label->setVisible(state);
+    m_search_count_label->setVisible(state);
 }
--- src/Panel.hpp
+++ src/Panel.hpp
@@ -12,13 +12,18 @@ class Panel : public QWidget
     void setTotalPageCount(const unsigned int &total) noexcept;
     void setFileName(const QString &name) noexcept;
     void setPageNo(const unsigned int &pageno) noexcept;
-
+    void setSearchCount(const unsigned int &count) noexcept;
+    void setSearchIndex(const unsigned int &index) noexcept;
+    void setSearchMode(const bool &state) noexcept;
 
 private:
     void initGui() noexcept;
     QLabel *m_filename_label = new QLabel();
     QLabel *m_pageno_label = new QLabel();
     QLabel *m_totalpage_label = new QLabel();
+    QLabel *m_search_count_label = new QLabel();
+    QLabel *m_search_index_label = new QLabel();
+    QLabel *m_search_label = new QLabel("Search: ");
     QHBoxLayout *m_layout = new QHBoxLayout();
 
 };
