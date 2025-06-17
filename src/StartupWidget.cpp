f730fcb361ccea41d77251f462a98d4ed37fe109
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-06-17 11:45:38 +0530

    Prettify startup page

--- src/StartupWidget.cpp
+++ src/StartupWidget.cpp
@@ -1,8 +1,24 @@
 #include "StartupWidget.hpp"
 
+#include <QLabel>
+#include <QLineEdit>
+#include <QSortFilterProxyModel>
+
 StartupWidget::StartupWidget(const QSqlDatabase &db, QWidget *parent) : QWidget(parent), m_db(db)
 {
-    QVBoxLayout *layout = new QVBoxLayout(this);
+    QVBoxLayout *layout  = new QVBoxLayout(this);
+    QLabel *recent_label = new QLabel("Recent Files");
+    QLineEdit *line_edit = new QLineEdit(this);
+    line_edit->setPlaceholderText("Search for file");
+
+    QFont font = recent_label->font();
+
+    font.setPixelSize(20);
+
+    recent_label->setFont(font);
+
+    layout->addWidget(recent_label);
+    layout->addWidget(line_edit);
     layout->addWidget(m_table_view);
 
     setProperty("tabRole", "startup");
@@ -21,18 +37,35 @@ StartupWidget::StartupWidget(const QSqlDatabase &db, QWidget *parent) : QWidget(
     m_model->select();
     m_table_view->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-    m_model->setHeaderData(m_model->fieldIndex("file_path"), Qt::Horizontal, tr("File Path"));
-    m_model->setHeaderData(m_model->fieldIndex("last_accessed"), Qt::Horizontal, tr("Last Visited"));
+    m_model->setHeaderData(m_model->fieldIndex("file_path"), Qt::Horizontal, "File");
+    // m_model->setHeaderData(m_model->fieldIndex("last_accessed"), Qt::Horizontal, tr("Last Visited"));
 
     // Optional: sort by last_accessed DESC
     m_model->setSort(m_model->fieldIndex("last_accessed"), Qt::DescendingOrder);
 
-    m_table_view->setModel(m_model);
+    QSortFilterProxyModel *proxy = new QSortFilterProxyModel(this);
+    proxy->setSourceModel(m_model);
+    proxy->setFilterKeyColumn(0); // Filter by title
+    proxy->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
+    proxy->setFilterRegularExpression(QRegularExpression(".*", QRegularExpression::CaseInsensitiveOption));
+
+    connect(line_edit, &QLineEdit::textChanged, this, [proxy](const QString &text)
+    {
+    proxy->setFilterRegularExpression(
+            text.isEmpty() ? QRegularExpression(".*", QRegularExpression::CaseInsensitiveOption)
+            : QRegularExpression(text, QRegularExpression::CaseInsensitiveOption)
+            );
+    });
+
+    m_table_view->setModel(proxy);
     m_table_view->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
-    m_table_view->horizontalHeader()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
     m_table_view->setSelectionBehavior(QAbstractItemView::SelectionBehavior::SelectRows);
 
+    m_table_view->horizontalHeader()->setVisible(false);
+
     m_table_view->setColumnHidden(1, true);
+    m_table_view->setColumnHidden(2, true);
 
     connect(m_table_view, &QTableView::doubleClicked, this, [this](const QModelIndex &index)
     {
