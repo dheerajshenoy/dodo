24251a79aa8db7d0b98e0534089bab187179e9d6
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-07-15 11:44:21 +0530

    Queue system to not overlap each message

--- src/MessageBar.cpp
+++ src/MessageBar.cpp
@@ -12,11 +12,29 @@ MessageBar::MessageBar(QWidget *parent) : QWidget(parent)
 void
 MessageBar::showMessage(const QString &msg, float sec) noexcept
 {
-    show();
+    m_queue.enqueue({.message = msg, .duration = sec});
+    if (!m_showing)
+        showNext();
+}
+
+void
+MessageBar::showNext() noexcept
+{
+    if (m_queue.isEmpty())
+    {
+        m_showing = false;
+        return;
+    }
+
+    m_showing             = true;
+    const auto [msg, sec] = m_queue.dequeue();
+
     m_label->setText(msg);
-    QTimer::singleShot(sec * 1000, [this]()
+    show();
+    QTimer::singleShot(sec * 1000, this, [this]()
     {
         hide();
         m_label->clear();
+        showNext(); // Show the next message after the current one
     });
 }
--- src/MessageBar.hpp
+++ src/MessageBar.hpp
@@ -3,6 +3,7 @@
 #include <QLabel>
 #include <QWidget>
 #include <QHBoxLayout>
+#include <QQueue>
 
 class MessageBar : public QWidget
 {
@@ -10,6 +11,15 @@ public:
     MessageBar(QWidget *parent = nullptr);
     void showMessage(const QString &msg, float sec = 2.0f) noexcept;
 
+    struct MessageItem
+    {
+        QString message;
+        float duration;
+    };
+
 private:
+    void showNext() noexcept;
+    bool m_showing { false };
+    QQueue<MessageItem> m_queue;
     QLabel *m_label{new QLabel()};
 };
