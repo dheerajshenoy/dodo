cedaa421f85c86f8e3a635975eb380e445a4ecfa
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-11-30 14:28:55 +0100

    Track selected state as member variable

--- src/Annotation.hpp
+++ src/Annotation.hpp
@@ -12,7 +12,7 @@ class Annotation : public QObject, public QGraphicsItem
 {
     Q_OBJECT
 public:
-    Annotation(int index, QGraphicsItem *parent = nullptr);
+    Annotation(int index, const QColor &color, QGraphicsItem *parent = nullptr);
 
     inline int index() noexcept
     {
@@ -21,16 +21,19 @@ public:
 
     void select(const QColor &color) noexcept;
     void restoreBrushPen() noexcept;
-    virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    virtual void paint(QPainter *painter,
+                       const QStyleOptionGraphicsItem *option, QWidget *widget);
 
 signals:
     void annotDeleteRequested(int index);
     void annotColorChangeRequested(int index);
+    void annotSelectRequested(int index);
 
 protected:
     int m_index{-1};
     QBrush m_brush{Qt::transparent};
     QPen m_pen{Qt::NoPen};
+    bool m_selected{false};
 
     QBrush m_originalBrush;
     QPen m_originalPen;
