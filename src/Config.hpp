977cc24144b9d1c2bc6bd8368b17dc7f7edc28b2
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-11-30 01:27:08 +0100

    Restructure config options

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,50 +1,61 @@
 #pragma once
 
 #include <QColor>
-#include <QMap>
+#include <QHash>
 
 struct Config
 {
-    QMap<QString, QColor> colors{};
-    QMap<QString, QString> shortcuts_map{};
+    QHash<QString, QString> shortcuts{};
 
-    float dpi{300};
-    float dpr{1.0f};
-    float inv_dpr{1.0f};
-    float zoom_by{1.25f};
-    float zoom{1.0};
-    float link_hint_size{0.5f};
+    struct ui
+    {
+        QHash<QString, QColor> colors{};
+        float link_hint_size{0.5f};
+        float zoom_by{1.25f};
+        float zoom{1.0};
+        bool menubar_shown{true};
+        bool panel_shown{true};
+        bool auto_resize{false};
+        bool link_boundary{false};
+        bool jump_marker_shown{true};
+        bool vscrollbar_shown{true};
+        bool hscrollbar_shown{true};
+        bool startup_tab{true};
+        bool outline_as_side_panel{true};
+        bool tabs_shown{true};
+        bool auto_hide_tabs{false};
+        int outline_panel_width{300};
+        int selection_drag_threshold{50};
+        QString window_title_format{"{} - dodo"};
+        bool full_filepath_in_panel{false};
+        QString initial_fit{"none"};
+        bool page_nav_with_mouse{false};
+    };
 
-    int startpage_override{-1};
-    int page_history_limit{5};
-    int selection_drag_threshold{50};
-    int cache_pages{10};
-    int antialiasing_bits{8};
-    int num_recent_files{10};
-    int outline_panel_width{300};
+    struct rendering
+    {
+        float dpi{300};
+        float dpr{1.0f};
+        float inv_dpr{1.0f};
+        bool icc_color_profile{true};
+        int antialiasing_bits{8};
+    };
 
+    struct behavior
+    {
+        int cache_pages{10};
+        // bool auto_reload{true};
+        bool invert_mode{false};
+        bool open_last_visited{false};
+        bool remember_last_visited{true};
+        bool recent_files{true};
+        int page_history_limit{5};
+        int num_recent_files{10};
+        int startpage_override{-1};
+        QString synctex_editor_command{QString()};
+    };
 
-    // bool auto_reload{true};
-    bool menubar_shown{true};
-    bool panel_shown{true};
-    bool icc_color_profile{true};
-    bool invert_mode{false};
-    bool page_nav_with_mouse{false};
-    bool jump_marker_shown{true};
-    bool full_filepath_in_panel{false};
-    bool open_last_visited{false};
-    bool auto_resize{false};
-    bool link_boundary{false};
-    bool remember_last_visited{true};
-    bool vscrollbar_shown{true};
-    bool hscrollbar_shown{true};
-    bool startup_tab{true};
-    bool recent_files{true};
-    bool outline_as_side_panel{true};
-    bool auto_hide_tabs{false};
-    bool tabs_shown{true};
-
-    QString initial_fit{"none"};
-    QString window_title_format{"{} - dodo"};
-    QString synctex_editor_command{QString()};
+    ui     ui{};
+    rendering rendering{};
+    behavior behavior{};
 };
