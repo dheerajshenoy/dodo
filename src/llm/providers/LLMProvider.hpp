9bd4dddc99a818e976e22f39a9293de7dafa0ec2
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-01-18 00:52:55 +0100

    Move model name from Request to Provider

--- src/llm/providers/LLMProvider.hpp
+++ src/llm/providers/LLMProvider.hpp
@@ -8,7 +8,6 @@ namespace LLM
 
 struct Request
 {
-    std::string model;
     std::string prompt;
     int max_tokens{512};
     // float temperature{0.7f}; //
@@ -21,15 +20,21 @@ public:
     explicit Provider(QObject *parent = nullptr);
     ~Provider() override;
 
-    inline void setSystemPrompt(const std::string &prompt)
+    inline void setSystemPrompt(const std::string &prompt) noexcept
     {
         m_system_prompt = prompt;
+    }
+
+    inline void setModel(const std::string &name) noexcept
+    {
+        m_model_name = name;
     }
 
     virtual void chat_stream(const Request &request) = 0;
 
 protected:
     std::string m_system_prompt{};
+    std::string m_model_name;
 
 signals:
     void dataReceived(const std::string &data);
